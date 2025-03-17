c5952f94ad1f318d2655b5894bdab4a4efc0f01d
Randy Lee <randy.lee@dot.state.fl.us>
2025-03-16 20:11:06 -0400

    tweak

--- RxTemplate/Components/Rx/Headless/AutoComplete/RxAutoComplete.cs
+++ RxTemplate/Components/Rx/Headless/AutoComplete/RxAutoComplete.cs
@@ -1,7 +1,7 @@
 namespace RxTemplate.Components.Rx.Headless.AutoComplete;
 
 public record RxAutoCompleteModel {
-    public HttpMethod OnSelectedHttpMethod { get; set; } = HttpMethod.Get;
+    public HttpMethod? OnSelectedHttpMethod { get; set; }
     public string OnSelectedEndpoint { get; set; } = null!;
     public string OnSelectedResponseTarget { get; set; } = null!;
     public string OnSelectedResponseSwap { get; set; } = null!;
