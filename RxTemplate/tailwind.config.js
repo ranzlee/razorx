de7a5eae58dbb7fdb11200d6c43f63186ba2aeaa
Randy Lee <randy.lee@dot.state.fl.us>
2025-03-24 20:42:26 -0400

    tailwind/daisy upgrade

--- RxTemplate/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["Components/**/*.razor"],
-  plugins: [require("@tailwindcss/typography")],
-  // [data-theme="<your selected dark theme name"]
-  darkMode: ["selector", '[data-theme="dark"]'],
-};
